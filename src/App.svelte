bfad7be8dc14cf1aa841c2d533662e91f0507c64
nanagami1369 <nanagami1369@gmail.com>
2023-08-19 18:29:08 +0900

    gridのcssをシンプルにした

--- src/App.svelte
+++ src/App.svelte
@@ -37,108 +37,111 @@
       </div>
     </div>
   </div>
-  <div class="control1">
-    <h2>animation-name</h2>
-    <p>@keyframes で定義したアニメーション名を記述します</p>
-  </div>
-  <div class="control2">
-    <h2>animation-duration</h2>
-    <p>何秒かけてアニメーションを1回するか指定します</p>
-    <label>
-      <input
-        type="number"
-        min="0.1"
-        bind:value={animationDuration}
-        step="0.1"
-      /><span>秒 </span></label
-    >
-  </div>
-  <div class="control3">
-    <h2>animation-timing-function</h2>
-    <p>何秒かけてアニメーションを1回するか指定します</p>
-    <p>
-      現在の設定:<span class="current-animation-timing-function"
-        >{animationTimingFunction}</span
-      >
-    </p>
-    <div class="ease-function-area">
-      <button on:click={() => (animationTimingFunction = "linear")}>
-        <h3>linear</h3>
-        <div class="animation-timing-function-container">
-          <div
-            class="animation-timing-function-bar animation-component"
-            style="animation-timing-function: linear;"
-          />
-        </div>
-      </button>
-      <button on:click={() => (animationTimingFunction = "ease")}>
-        <h3>ease</h3>
-        <div class="animation-timing-function-container">
-          <div
-            class="animation-timing-function-bar animation-component"
-            style="animation-timing-function: ease;"
-          />
-        </div>
-      </button>
-      <button on:click={() => (animationTimingFunction = "ease-in")}>
-        <h3>ease-in</h3>
-        <div class="animation-timing-function-container">
-          <div
-            class="animation-timing-function-bar animation-component"
-            style="animation-timing-function: ease-in;"
-          />
-        </div>
-      </button>
-      <button on:click={() => (animationTimingFunction = "ease-out")}>
-        <h3>ease-out</h3>
-        <div class="animation-timing-function-container">
-          <div
-            class="animation-timing-function-bar animation-component"
-            style="animation-timing-function: ease-out;"
-          />
-        </div>
-      </button>
-      <button on:click={() => (animationTimingFunction = "ease-in-out")}>
-        <h3>ease-in-out</h3>
-        <div class="animation-timing-function-container">
-          <div
-            class="animation-timing-function-bar animation-component"
-            style="animation-timing-function: ease-in-out;"
-          />
-        </div>
-      </button>
+  <div class="control">
+    <div>
+      <h2>animation-name</h2>
+      <p>@keyframes で定義したアニメーション名を記述します</p>
     </div>
-  </div>
-  <div class="control4">
-    <h2>animation-delay</h2>
-    <p>アニメーションをいつ実行するか設定します</p>
-    <button on:click={restartAnimation}>再起動</button>
-    <label>
-      <input
-        type="number"
-        min="0"
-        bind:value={animationDelay}
-        step="0.1"
-      /><span>秒 </span></label
-    >
-  </div>
-  <div class="control5">
-    <h2>animation-iteration-count</h2>
-    <p>アニメーション回数を設定します</p>
-    <p>
-      {`${animationIterationCount}回`}
-    </p>
-    <button on:click={restartAnimation}>再起動</button>
-    <label>
-      <input
-        type="number"
-        min="0.1"
-        bind:value={animationIterationCount}
-        step="0.1"
-      /><span>回 </span></label
-    >
-    <button on:click={() => (animationIterationCount = "infinite")}>無限</button
-    >
+    <div>
+      <h2>animation-duration</h2>
+      <p>何秒かけてアニメーションを1回するか指定します</p>
+      <label>
+        <input
+          type="number"
+          min="0.1"
+          bind:value={animationDuration}
+          step="0.1"
+        /><span>秒 </span></label
+      >
+    </div>
+    <div>
+      <h2>animation-timing-function</h2>
+      <p>何秒かけてアニメーションを1回するか指定します</p>
+      <p>
+        現在の設定:<span class="current-animation-timing-function"
+          >{animationTimingFunction}</span
+        >
+      </p>
+      <div class="ease-function-area">
+        <button on:click={() => (animationTimingFunction = "linear")}>
+          <h3>linear</h3>
+          <div class="animation-timing-function-container">
+            <div
+              class="animation-timing-function-bar animation-component"
+              style="animation-timing-function: linear;"
+            />
+          </div>
+        </button>
+        <button on:click={() => (animationTimingFunction = "ease")}>
+          <h3>ease</h3>
+          <div class="animation-timing-function-container">
+            <div
+              class="animation-timing-function-bar animation-component"
+              style="animation-timing-function: ease;"
+            />
+          </div>
+        </button>
+        <button on:click={() => (animationTimingFunction = "ease-in")}>
+          <h3>ease-in</h3>
+          <div class="animation-timing-function-container">
+            <div
+              class="animation-timing-function-bar animation-component"
+              style="animation-timing-function: ease-in;"
+            />
+          </div>
+        </button>
+        <button on:click={() => (animationTimingFunction = "ease-out")}>
+          <h3>ease-out</h3>
+          <div class="animation-timing-function-container">
+            <div
+              class="animation-timing-function-bar animation-component"
+              style="animation-timing-function: ease-out;"
+            />
+          </div>
+        </button>
+        <button on:click={() => (animationTimingFunction = "ease-in-out")}>
+          <h3>ease-in-out</h3>
+          <div class="animation-timing-function-container">
+            <div
+              class="animation-timing-function-bar animation-component"
+              style="animation-timing-function: ease-in-out;"
+            />
+          </div>
+        </button>
+      </div>
+    </div>
+    <div>
+      <h2>animation-delay</h2>
+      <p>アニメーションをいつ実行するか設定します</p>
+      <button on:click={restartAnimation}>再起動</button>
+      <label>
+        <input
+          type="number"
+          min="0"
+          bind:value={animationDelay}
+          step="0.1"
+        /><span>秒 </span></label
+      >
+    </div>
+    <div>
+      <h2>animation-iteration-count</h2>
+      <p>アニメーション回数を設定します</p>
+      <p>
+        {`${animationIterationCount}回`}
+      </p>
+      <button on:click={restartAnimation}>再起動</button>
+      <label>
+        <input
+          type="number"
+          min="0.1"
+          bind:value={animationIterationCount}
+          step="0.1"
+        /><span>回 </span></label
+      >
+      <button on:click={() => (animationIterationCount = "infinite")}
+        >無限</button
+      >
+    </div>
   </div>
 </main>
 
@@ -172,16 +175,12 @@
   main {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: 3em repeat(4, max-content);
+    grid-template-rows: 3em max-content;
     grid-column-gap: 0px;
     grid-row-gap: 0px;
     grid-template-areas:
       "title title"
-      "control1 animation-viewer"
-      "control2 animation-viewer"
-      "control3 animation-viewer"
-      "control4 animation-viewer"
-      "control5 animation-viewer";
+      "control animation-viewer";
   }
 
   .title {
@@ -192,20 +191,12 @@
     grid-area: animation-viewer;
   }
 
-  .control1 {
-    grid-area: control1;
+  .control {
+    grid-area: control;
   }
 
   input[type="number"]:invalid {
     border: solid red;
-  }
-
-  .control2 {
-    grid-area: control2;
-  }
-
-  .control3 {
-    grid-area: control3;
   }
 
   .ease-function-area {
@@ -247,14 +238,6 @@
     }
   }
   /* animation timing function animation end*/
-
-  .control4 {
-    grid-area: control4;
-  }
-
-  .control5 {
-    grid-area: control5;
-  }
 
   /* progress bar animation start */
   .progress-bar-container {
